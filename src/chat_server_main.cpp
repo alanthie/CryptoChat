07f02dcf71c71d24a8ee6f33e5164e7c31146292
Alain Lanthier <alanthier001@hotmail.ca>
2024-11-25 07:19:31 -0500

    misc

--- src/chat_server_main.cpp
+++ src/chat_server_main.cpp
@@ -57,7 +57,9 @@ int main(int argc, char** argv)
     {
         std::cout << "enter command: ";
         std::cin >> entry;
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        if (entry == "quit")
+            signalHandler(0);
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     t.join();
 }
